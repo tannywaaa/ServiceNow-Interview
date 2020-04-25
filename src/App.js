6c95c82a1561e19838e4031b2bc64250c511f445
Tangni Wang <twang63@ucsc.edu>
2020-04-24 21:13:27 -0700

    api get request successfully imported and displayed in the table

--- src/App.js
+++ src/App.js
@@ -3,48 +3,63 @@ import "./App.css";
 import { Table } from "react-bootstrap";
 import Card from "./components/Card";
 
+const URL =
+  "https://servicenow-ui-coding-challenge-api.netlify.app/.netlify/functions/server/incidents";
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      incidents_all: []
+    };
+  }
+  async componentDidMount() {
+    const res = await fetch(URL);
+    const api = await res.json();
+    const incidents_all = api;
+    this.setState({ incidents_all });
+  }
   render() {
+    let displayrows = [];
+    let item = this.state.incidents_all;
+
+    for (let i = 0; i < item.length; i++) {
+      let temp_row_element = (
+        <tr>
+          <td>{item[i].number}</td>
+          <td>{item[i].priority}</td>
+          <td>{item[i].short_description}</td>
+          <td>{item[i].category}</td>
+          <td>{item[i].state}</td>
+          <td>{item[i].sys_created_on}</td>
+        </tr>
+      );
+      displayrows.push(temp_row_element);
+    }
     return (
       <div className="App">
         <h3>At A Glance</h3>
         <Card title={"aaa"} />
         <h3>All Incidents</h3>
-        {this.incident_table()}
+        {this.incident_table(displayrows)}
       </div>
     );
   }
-  incident_table = () => {
+  incident_table = row_item => {
     return (
       <div class="incident_table">
         <Table striped bordered hover size="sm">
           <thead>
             <tr>
-              <th>#</th>
-              <th>First Name</th>
-              <th>Last Name</th>
-              <th>Username</th>
+              <th>Number</th>
+              <th>Priority</th>
+              <th>Short description</th>
+              <th>Category</th>
+              <th>State</th>
+              <th>Created</th>
             </tr>
           </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>Mark</td>
-              <td>Otto</td>
-              <td>@mdo</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>Jacob</td>
-              <td>Thornton</td>
-              <td>@fat</td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td colSpan="2">Larry the Bird</td>
-              <td>@twitter</td>
-            </tr>
-          </tbody>
+          <tbody>{row_item}</tbody>
         </Table>
       </div>
     );
